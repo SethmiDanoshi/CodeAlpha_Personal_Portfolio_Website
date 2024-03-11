184e9889813fd8ece98abba7655d2bf2cec41b33
SethmiDanoshi <111180840+SethmiDanoshi@users.noreply.github.com>
2024-03-11 21:54:38 +0530

    Task 01

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing animation */
+
+var typed= new Typed(".typing",{
+    strings:["","Web Designer","Graphic Designer","Web Developer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
